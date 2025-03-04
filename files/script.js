6c34b65af5078be638626c6644d5e004487d363e
Davide0033 <70667149+davide0033@users.noreply.github.com>
2025-03-04 18:44:08 +0100

    small cosmetic update

---
+++ files/script.js
@@ -0,0 +1,19 @@
+document.getElementById("textToCopy").addEventListener("click", function() {
+    const text = "newnova.blockworlds.io"
+    const btn = document.getElementById("textToCopy")
+    
+
+    navigator.clipboard.writeText(text).then(() => {
+        btn.classList.add("copied", "important")
+        btn.innerHTML = "Copied to clipBoard!"
+
+      setTimeout(() => {
+         btn.classList.remove("copied")
+        
+        
+        btn.innerHTML = "IP: newnova.blockworlds.io"
+      }, 2000);
+    }).catch(err => {
+      console.error("Errore nella copia:", err);
+    });
+  });
