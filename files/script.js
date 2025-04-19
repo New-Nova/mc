9444f39dfce548df6f7c15a0c09b622c9780ef2e
Davide0033 <70667149+davide0033@users.noreply.github.com>
2025-04-19 22:24:41 +0200

    New crafting recepies, small edit to all html

--- files/script.js
+++ files/script.js
@@ -1,4 +1,4 @@
-document.getElementById("textToCopy").addEventListener("click", function() {
+document.getElementById("textToCopy")?.addEventListener("click", function() {
     const text = "newnova.blockworlds.io"
     const btn = document.getElementById("textToCopy")
     
@@ -17,3 +17,24 @@ document.getElementById("textToCopy").addEventListener("click", function() {
       console.error("Errore nella copia:", err);
     });
   });
+
+
+  async function getOnline() {
+    let response = await fetch("http://newnova.blockworlds.io:8084/v2/newnova/");
+    let response2 = await fetch("http://newnova.blockworlds.io:8085/v2/newnova/")
+    if (response.ok || response2.ok) {
+      let pl = await response.json();
+      let pl2 = await response2.json();
+      let totPla = pl.players.numOnlinePlayers + pl2.players.numOnlinePlayers
+      let pla
+      if(totPla == 1)
+        pla = "player"
+      else pla = "players"
+      return "Currently " + totPla +" "+ pla+" online";
+    } else {
+      return "there has been an error";
+    }
+  }
+
+
+ getOnline().then(ret => document.getElementById("players").innerHTML = ret)
